c57b485ce384fe491b0df923f1c21f13cace1590
Kaloyan Yanev <kaloyan.yanev98@gmail.com>
2016-05-06 18:17:09 +0300

    More HomeWork

---
+++ Arrays/BinarySearch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int arrSize = int.Parse(Console.ReadLine());
+        int[] intArr = new int[arrSize];
+
+        for (int i = 0; i < arrSize; i++)
+        {
+            intArr[i] = int.Parse(Console.ReadLine());
+        }
+        int index = int.Parse(Console.ReadLine());
+        int numInIndex = Array.BinarySearch(intArr, index);
+        if (numInIndex < 0)
+        {
+            numInIndex = -1;
+        }
+        Console.WriteLine(numInIndex);
+
+    }
+}
+
+
---
+++ Arrays/IndexOfLetters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char[] alphabet = new char[26];
+        for (int i = 97; i < 123; i++)
+        {
+            alphabet[i - 97] = (char)i;
+        }
+        string input = Console.ReadLine();
+
+        for (int j = 0; j < input.Length; j++)
+        {
+            for (int k = 0; k < 26; k++)
+            {
+                if (input[j] == alphabet[k])
+                {
+                    Console.WriteLine(k);
+                    break;
+                }
+            }
+        }
+    }
+}
+
