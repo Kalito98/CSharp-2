8c4a74c25e422a5362eb5a0141dff38b41086e92
Kaloyan Yanev <kaloyan.yanev98@gmail.com>
2016-05-16 14:45:42 +0300

    Homework

---
+++ Methods/Methods/IntegerCalculations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Minimum(int[] numbers)
+    {
+        Array.Sort(numbers);
+        Console.WriteLine(numbers[0]);
+    }
+    static void Maximum(int[] numbers)
+    {
+        Array.Sort(numbers);
+        Console.WriteLine(numbers[4]);
+    }
+    static void Avarage(int[] numbers)
+    {
+        double result = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            result += numbers[i];
+        }
+        result /= numbers.Length;
+        Console.WriteLine("{0:F2}", result);
+    }
+    static void Sum(int[] numbers)
+    {
+        double result = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            result += numbers[i];
+        }
+        Console.WriteLine(result);
+    }
+    static void Product(int[] numbers)
+    {
+        BigInteger result = 1;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            result *= numbers[i];
+        }
+        Console.WriteLine(result);
+    }
+    static void Main()
+    {
+        string numbers = Console.ReadLine();
+        string[] numbersStrArr = numbers.Split(' ');
+        int[] numbersIntArr = Array.ConvertAll<string, int>(numbersStrArr, int.Parse);
+        Minimum(numbersIntArr);
+        Maximum(numbersIntArr);
+        Avarage(numbersIntArr);
+        Sum(numbersIntArr);
+        Product(numbersIntArr);
+    }
+}
+
--- Methods/Methods/NFactorial/Program.cs
+++ Methods/Methods/NFactorial/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
+using System.Numerics;
 
 class Program
 {
-    static void Factorial()
+    static void Factorial(int number)
     {
-
+        BigInteger result = number;
+        for (int i = 1; i < number; i++)
+        {
+            result *= i;
+        }
+        if (number == 0)
+        {
+            result = 1;
+        }
+        Console.WriteLine(result);
     }
 
     static void Main()
     {
-
+        Factorial(int.Parse(Console.ReadLine()));
     }
 }
 
