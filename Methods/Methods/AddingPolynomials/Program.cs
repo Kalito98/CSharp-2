38e4025b76ea97eb7ea5ab0bef08cafc6a460092
Kaloyan Yanev <kaloyan.yanev98@gmail.com>
2016-05-18 13:23:13 +0300

    Homework

---
+++ Methods/Methods/AddingPolynomials/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Polynomials(int size, string firstArr, string secondArr)
+    {
+        string[] firstStringArr = firstArr.Split(' ');
+        string[] secondStringArr = secondArr.Split(' ');
+        int[] firstIntArr = Array.ConvertAll<string, int>(firstStringArr, int.Parse);
+        int[] secondIntArr = Array.ConvertAll<string, int>(secondStringArr, int.Parse);
+
+        for (int i = 0; i < firstIntArr.Length; i++)
+        {
+            firstIntArr[i] += secondIntArr[i];
+        }
+        for (int i = 0; i < firstIntArr.Length; i++)
+        {
+            Console.Write(firstIntArr[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+        string firstArr = Console.ReadLine();
+        string secondArr = Console.ReadLine();
+        Polynomials(size, firstArr, secondArr);
+    }
+}
+
---
+++ NumeralSystems/BinaryShort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        short number = short.Parse(Console.ReadLine());
+        Console.WriteLine(Convert.ToString(number, 2).PadLeft(16, '0'));
+    }
+}
+
+
